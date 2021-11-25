27001f79c53e0741295a967c367ed44f1f0fca1b
Luis Lugo <luisclugop@gmail.com>
2021-11-24 19:47:01 -0600

    Desafio 3 - Servidores web - Leyendo archivo, y leyendo archivo random por id

--- app.js
+++ app.js
@@ -4,12 +4,32 @@ const express = require("express")
 const fs = require("fs")
 
 const app = express()
-const port = 3000
-let visitas = 0
+const port = 8080
+// let visitas = 0
+
+let filenameOriginal = "productos.txt";
+
+let productos = [
+    {
+        "id": 1,
+        "product": "lapiz",
+        "price": 20,
+    },
+    {
+        "id": 2,
+        "product": "lapicero",
+        "price": 25,
+    },
+    {
+        "id": 3,
+        "product": "pluma",
+        "price": 30,
+    },
+];
 
 app.listen(port, () => {
     try {
-        let data = fs.readFileSync("productos.txt")
+        let data = fs.readFileSync(filenameOriginal)
     } catch(e) { 
         console.log("No file")
     }
@@ -17,15 +37,25 @@ app.listen(port, () => {
 })
 
 app.get('/', (request, response) => {
-    response.send("Bienvenido al puerto 3000")
+    response.send("Bienvenido al puerto 8080")
 })
 
 app.get('/productos', (request, response) => {
-    visitas++;
-    fs.writeFileSync( "visitas", visitas.toString() )
-    response.send(`Las visitas son ${visitas}`)
+    let dataString = JSON.stringify(productos);
+    fs.writeFileSync( filenameOriginal, dataString )
+    response.send(`Los productos son ${dataString}`)
 })
 
-app.get('/productoRandom', (request, response) => {
-    response.send("Bienvenido a el Producto que escogiste")
+// app.get('/productos', (request, response) => {
+//     visitas++;
+//     fs.writeFileSync( "visitas", visitas.toString() )
+//     response.send(`Las visitas son ${visitas}`)
+// })
+
+app.get('/productoRandom/:id', (request, response) => {
+    console.log("Get Peticion Recibida")
+
+    let id = request.params.id;
+    let producto = productos.find( product => product.id == id)
+    response.send(producto)
 })
